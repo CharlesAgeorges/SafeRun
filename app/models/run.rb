a1268c61ddb50a9999ac751dc7847646542b22c6
Charles AGEORGES <charles.ageorges@gmail.com>
2025-12-09 16:06:18 +0100

    runs controller/new/indew/show

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,7 +1,9 @@
 class Run < ApplicationRecord
   belongs_to :user
   validates :duration, :distance, :status, :start_point, presence: true
-  has_many :positions, :incidents, :guardian_notifications, dependent: :destroy
+  has_many :positions, dependent: :destroy
+  has_many :incidents, dependent: :destroy
+  has_many :guardian_notifications, dependent: :destroy
   has_many :run_badges
   has_many :badges, through: :run_badges
 end

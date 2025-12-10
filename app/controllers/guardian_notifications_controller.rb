af71109cfc7b1e22d6acd073590522f1dbea03ea
Brendan Le Sage <byrdlesage@gmail.com>
2025-12-10 17:52:54 +0100

    ok

--- app/controllers/guardian_notifications_controller.rb
+++ app/controllers/guardian_notifications_controller.rb
@@ -1,12 +1,16 @@
 class GuardianNotificationsController < ApplicationController
+  before_action :client, only: [:run_start_alert]
+
   def run_start_alert
-    @run = current_user.runs.find(params[:run_id])
+    @run = current_user.runs.find(params[:id])
     @guardians = @run.guardians
-    client.messages.create(
-      from: "#{client_twilio_phone}",
-      to: "#{@guardians.phone_number}",
-      body: "#{current_user.name} started running in #{@run.start_point}, and is supposed to run for #{@run.duration}, look for news from them after the time went by!"
-    )
+    @guardians.each do |guardian|
+      client.messages.create(
+        from: @client_twilio_phone,
+        to: guardian.phone_number,
+        body: "#{current_user.name} a démarré une run #{@run.start_point}, qui devrait durer #{@run.duration}minutes, jètes un oeil à tes notifs le moment venu pour savoir si tout est ok!"
+      )
+    end
   end
 
   def run_end_alert
@@ -24,10 +28,10 @@ class GuardianNotificationsController < ApplicationController
 
 
   def client
-  account_sid = TWILIO_ACCOUNT_SID
-  auth_token = TWILIO_AUTH_TOKEN
-  client_twilio_phone = TWILIO_PHONE_NUMBER
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    @account_sid = ENV['TWILIO_ACCOUNT_SID']
+    @auth_token = ENV['TWILIO_AUTH_TOKEN']
+    @client_twilio_phone = ENV['TWILIO_PHONE_NUMBER']
+    @client = Twilio::REST::Client.new @account_sid, @auth_token
   end
 
 end

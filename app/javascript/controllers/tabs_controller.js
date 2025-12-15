6064eb26287f22ec4832e4cbdc0ab3324e4c618b
Brendan Le Sage <byrdlesage@gmail.com>
2025-12-15 09:47:27 +0100

    ok

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -6,7 +6,27 @@ export default class extends Controller {
   // connect() {
   //   console.log(this.hideTargets);
   // }
-    toggle() {
-      
+    toggle(event) {
+      const clickedTab = event.currentTarget.dataset.tabsTarget
+
+      // Cache tous les contenus et désactive les boutons
+      this.runsTargets.forEach(el => {
+        el.classList.remove("active")
+        if (el.dataset.content) el.style.display = "none"
+      })
+      this.guardiansTargets.forEach(el => {
+        el.classList.remove("active")
+        if (el.dataset.content) el.style.display = "none"
+      })
+      this.infosTargets.forEach(el => {
+        el.classList.remove("active")
+        if (el.dataset.content) el.style.display = "none"
+      })
+
+      // Active le bouton cliqué et affiche son contenu
+      this[`${clickedTab}Targets`].forEach(el => {
+        el.classList.add("active")
+        if (el.dataset.content) el.style.display = "block"
+      })
     }
 }

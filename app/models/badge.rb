d38345d3bb9c89a98033f6789062376f02f4f6eb
Brendan Le Sage <byrdlesage@gmail.com>
2025-12-17 09:35:02 +0100

    badge ok

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,7 @@
 class Badge < ApplicationRecord
   has_many :run_badges
   has_many :runs, through: :run_badges
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
 end

f72a599146925ff9776afe3b3924c62544d67a77
Steve Morin <steve.morin@outlook.fr>
2025-12-18 11:07:27 +0100

    pop up safeRun

---
+++ app/javascript/controllers/make_public_modal_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["confirmBtn"]
+
+  connect() {
+    this.element.addEventListener("show.bs.modal", this.updateUrl.bind(this))
+  }
+
+  updateUrl(event) {
+    const button = event.relatedTarget
+    if (button) {
+      const url = button.getAttribute("data-make-public-url")
+      if (url && this.hasConfirmBtnTarget) {
+        this.confirmBtnTarget.setAttribute("href", url)
+      }
+    }
+  }
+}
